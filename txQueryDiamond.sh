0f1150d93f0f8d516eb1fa9df0a2164ea17cfa95
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-01-12 20:24:16 +0200

    Create txQueryDiamond.sh

---
+++ txQueryDiamond.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+. 0_configVariables.sh
+
+# 2) Query the diamond.
+sudo docker exec \
+	-e "CORE_PEER_LOCALMSPID=$MSP_ID" \
+	-e "CORE_PEER_MSPCONFIGPATH=$MSP_CONFIG_PATH_CLI" \
+	$CLI_PEER \
+		peer chaincode query \
+			-C $CHANNEL_ID \
+			-n diamonds \
+			-c '{"Args":["queryDiamond","Kohinoor"]}'
+      
