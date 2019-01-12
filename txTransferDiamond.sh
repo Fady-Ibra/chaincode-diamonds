8962b8351171cd18dd01138be519d70a2dadea9a
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-01-12 20:25:24 +0200

    Create txTransferDiamond.sh

---
+++ txTransferDiamond.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+. 0_configVariables.sh
+
+# 3) Transfer the diamond.
+sudo docker exec \
+	-e "CORE_PEER_LOCALMSPID=$MSP_ID" \
+	-e "CORE_PEER_MSPCONFIGPATH=$MSP_CONFIG_PATH_CLI" \
+	$CLI_PEER \
+		peer chaincode invoke \
+			-o $ORDER_PEER:7050 \
+			-C $CHANNEL_ID \
+			-n diamonds \
+			-c '{"Args":["transferDiamond","Kohinoor","Victoria"]}'
