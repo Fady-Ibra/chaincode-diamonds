f88160be331eafd7253c5653f0dd6af312cf256a
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-01-12 19:13:09 +0200

    Update installFabric.sh

--- installFabric.sh
+++ installFabric.sh
@@ -10,13 +10,13 @@ mkdir -p ~/go/work/src/github.com/hyberledger
 echo 'export GOPATH=~/go/work' >> ~/.profile
 source ~/.profile
 cd ~/go/work/src/github.com/hyberledger
-git clone https://github.com/hyperledger/fabric-samples.git
+git clone https://github.com/hyperledger/fabric-samples
 
 cd fabric-samples
 sudo ./scripts/bootstrap.sh
 
 mkdir -p chaincode/diamonds/
-mv diamonds.go chaincode/diamonds/
+mv ~/chaincode-diamonds/diamonds.go chaincode/diamonds/
 
 cd basic-network/
 sudo docker-compose up -d
