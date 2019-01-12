b7c402ac8e2663bd433bb53ff8c7a99c61527c6a
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2019-01-12 20:21:08 +0200

    Create CreateDiamond.sh

---
+++ CreateDiamond.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+. 0_configVariables.sh
+
+# 1) Create a diamond.
+sudo docker exec \
+	-e "CORE_PEER_LOCALMSPID=$MSP_ID" \
+	-e "CORE_PEER_MSPCONFIGPATH=$MSP_CONFIG_PATH_CLI" \
+	$CLI_PEER \
+		peer chaincode invoke \
+			-o $ORDER_PEER:7050 \
+			-C $CHANNEL_ID \
+			-n diamonds \
+			-c '{"Args":["createDiamond","Kohinoor","India","105","Albert"]}'
